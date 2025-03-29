e3d9695522495879e3a18f84e38342edbb76c1ea
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-03-29 17:29:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-API_ID = 123456  # Replace with your actual API ID
-API_HASH = "your_api_hash"  # Replace with your actual API Hash
-BOT_TOKEN = "your_bot_token"  # Replace with your bot token
-MONGO_URI = "mongodb+srv://your_mongo_uri"  # Replace with your MongoDB URI
-FORCE_SUB_CHANNEL = "YourChannelUsername"  # Replace with your channel username
+API_ID = 123456  
+API_HASH = "your_api_hash"  
+BOT_TOKEN = "your_bot_token"  
+MONGO_URI = "mongodb+srv://your_mongo_uri"  
+FORCE_SUB_CHANNEL = "YourChannelUsername"  
