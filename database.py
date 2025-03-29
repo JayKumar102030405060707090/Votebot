b57bb362a7b7aafd85c0649f93052a62449bef49
JayKumar102030405060707090 <166695990+JayKumar102030405060707090@users.noreply.github.com>
2025-03-29 17:44:10 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,16 +1,15 @@
-from pymongo import MongoClient
-
 class Database:
-    def __init__(self, uri):
-        self.client = MongoClient(uri)
-        self.db = self.client["VoteBotDB"]
-        self.votes = self.db["votes"]
+    def __init__(self):
+        self.votes = {}
 
     def add_vote(self, user_id, poll_id):
-        self.votes.update_one({"poll_id": poll_id, "user_id": user_id}, {"$set": {"vote": 1}}, upsert=True)
+        if poll_id not in self.votes:
+            self.votes[poll_id] = set()
+        self.votes[poll_id].add(user_id)
 
     def remove_vote(self, user_id, poll_id):
-        self.votes.delete_one({"poll_id": poll_id, "user_id": user_id})
+        if poll_id in self.votes and user_id in self.votes[poll_id]:
+            self.votes[poll_id].remove(user_id)
 
     def get_votes(self, poll_id):
-        return self.votes.count_documents({"poll_id": poll_id})
+        return len(self.votes.get(poll_id, set()))
